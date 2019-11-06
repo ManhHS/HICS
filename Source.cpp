ec33617876d20dd1a13527b77534518cf8f02e2a
ManhHS <hasonmanh101@gmail.com>
2019-11-06 23:47:35 +0700

    add a.txt and Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include "MainFrom.h"
+
+using namespace std;
+
+int main()
+{
+	//System::Windows::Forms::Application::Run(gcnew Test::MainFrom);
+	int a = 1, b = 2, c = 3;
+
+
+	int* arr[3];
+	for (int i = 0; i < 2;i++)
+	{
+		arr[i] = new int;
+	}
+	arr[0] = &a;
+	arr[1] = &b;
+	arr[2] = &c;
+
+	for (int i = 0; i <= 1; i++)
+	{
+		arr[i] = arr[i + 1];
+	}
+	delete arr[2];
+	arr[2] = NULL;
+
+	system("pause");
+	return 0;
+}
